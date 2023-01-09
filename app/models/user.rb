a8c7762b6482141bea61c12f9eb6b0431cb82951
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-09 07:44:15 +0000

    [Update]registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
+  
+  
  has_one_attached :profile_image
 
   def get_profile_image(width, height)
