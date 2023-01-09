ff291fcb2afbbfa65f59b6bd1b25ac795a6f281d
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-09 09:18:18 +0000

    [Update]registration,session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  def after_sign_in_path_for(resource)
+    my_page_users_path
+  end
+
+  protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:name])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  
-  
+
+  validates :name, presence: true
+
  has_one_attached :profile_image
 
   def get_profile_image(width, height)
