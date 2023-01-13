1189126bcbeb00e88ca116073c472be15bc3041a
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-13 09:19:58 +0000

    [Update]bootstrap導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
 import '@fortawesome/fontawesome-free/js/all'
 import "../stylesheets/application"
 import "../stylesheets/style.css"
