b5e8c45a227937861592738656cd174224bcc516
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-16 07:29:34 +0000

    [Update]フォロー・フォロワー一覧レイアウト調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "channels"
 import '@fortawesome/fontawesome-free/js/all'
 import "../stylesheets/application"
 import "../stylesheets/header_footer.css"
+import "../stylesheets/relationships.css"
 import "../stylesheets/style.css"
 
 Rails.start()
