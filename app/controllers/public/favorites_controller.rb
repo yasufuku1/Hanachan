f0c775a0111adc71eae5b113956efd8f1338a92d
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-19 07:01:32 +0000

    [Update]いいね通知機能追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -5,12 +5,14 @@ class Public::FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
+    favorite.create_notification_like!(current_user)
     redirect_to post_path(post)
   end
 
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
+    Notification.find_by(post_id: post.id).destroy
     favorite.destroy
     redirect_to post_path(post)
   end
