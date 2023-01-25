19634c1fe22f4a463084ea14bb14ef26cfe4c4ee
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-25 02:29:12 +0000

    [Update]users/_index.html.erb追加,不要コード削除

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,6 @@
 class Notification < ApplicationRecord
-  # スコープ(新着順)
+  # デフォルトの並び順を「作成日時の降順」で指定
   default_scope->{order(created_at: :desc)}
-  # belongs_to :post, optional: true #nil を許可する
-  # belongs_to :favorite, optional: true
 
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
