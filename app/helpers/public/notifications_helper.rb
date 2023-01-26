c5840e4e222f7182a0a06190a48008a2d683dac4
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-26 03:02:46 +0000

    [Update]users/_index.html.erbのエラー修正、未通知確認ヘルパーメソッド追加

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -1,2 +1,7 @@
 module Public::NotificationsHelper
+
+  # ログインしているユーザに未通知があるかどうか調べる
+  def unchecked_notifications
+    @notifications = current_user.passive_notifications.where(checked: false)
+  end
 end
