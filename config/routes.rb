2b9581c25c67fd6c63c1571295b8696b78845ab4
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2023-01-27 00:50:53 +0000

    [Update]ユーザ新規登録失敗後リダイレクトエラー対策追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+    # ユーザ登録失敗後のリダイレクトエラー対策
+    get 'users', to: 'public/registrations#new'
   end
   scope module: :public do
     root to: 'homes#top'
